089649661ff4030518bb8c1258dd2c6a2222d964
Carlos Alvarado Robles <carlos2000emilioa@gmail.com>
2023-03-10 13:43:11 -0500

    Edición y Eliminación de Tipos de Pagos

--- app/Http/Livewire/ReservasAdmin/TipoPagosCuentas/TipoPagosCuentas.php
+++ app/Http/Livewire/ReservasAdmin/TipoPagosCuentas/TipoPagosCuentas.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\ReservasAdmin\TipoPagosCuentas;
 
 use App\Models\Reservas\CuentaPagos;
+use App\Models\Reservas\Pagos;
 use App\Models\Reservas\TipoPagos;
 use Livewire\Component;
 
@@ -10,57 +11,117 @@ class TipoPagosCuentas extends Component
 {
     /**ATRIBUTOS DE TIPOS DE PAGO */
     public $idTipoPago, $nombre_tipo_pago;
-    public $title='CREAR TIPOS DE CUENTAS BANCARIAS';
+    public $title = 'CREAR TIPOS DE CUENTAS BANCARIAS';
     /**ATRIBUTOS DE CUENTAS BANCARIAS */
-    public $numero_cuenta,$title2 ='';
+    public $numero_cuenta, $title2 = '';
+
+    protected $listeners = ['deleteTipoPago', 'deleteAtractivo' => 'deleteAtractivo'];
+
+
     public function render()
     {
         if ($this->idTipoPago) {
             //Cuentas Bancarias
-            $cuentas = CuentaPagos::select('id','numero_cuenta')
-            ->where('tipo_pagos_id', $this->idTipoPago)
-            ->get();
+            $cuentas = CuentaPagos::select('id', 'numero_cuenta')
+                ->where('tipo_pagos_id', $this->idTipoPago)
+                ->get();
         } else {
             # code...
             $cuentas = [];
         }
 
-        
-        $tipo_pagos = TipoPagos::all(['id','nombre_tipo_pago']);
-        return view('livewire.reservas-admin.tipo-pagos-cuentas.tipo-pagos-cuentas', compact('tipo_pagos','cuentas'));
+
+        $tipo_pagos = TipoPagos::all(['id', 'nombre_tipo_pago']);
+        return view('livewire.reservas-admin.tipo-pagos-cuentas.tipo-pagos-cuentas', compact('tipo_pagos', 'cuentas'));
     }
 
-    public function saveTipoPago(){
+    function resetUI(){
+
+    }
+    public function saveTipoPago()
+    {
         $this->validate([
             'nombre_tipo_pago' => 'required|string|min:2'
         ]);
-        $tipo_pago = TipoPagos::create(
-            [
-                'nombre_tipo_pago' => $this->nombre_tipo_pago
-            ]
-        );
+        if ($this->idTipoPago) {
+            # Actualizar
+            $tipo_pago = TipoPagos::find($this->idTipoPago);
+            //dd($this->idTipoPago);
+            $tipo_pago->nombre_tipo_pago = $this->nombre_tipo_pago;
+            $tipo_pago->save();
+            $msg = 'Tipo de Pago Actualizada Correctamente';
+        } else {
+            # Crear
+            $tipo_pago = TipoPagos::create(
+                [
+                    'nombre_tipo_pago' => $this->nombre_tipo_pago
+                ]
+            );
+            $msg = 'Tipo de Cuenta Creada Correctamente';
+        }
 
         $this->dispatchBrowserEvent('swal', [
             'title' => 'MUY BIEN !',
             'icon' => 'success',
-            'text' => 'Tipo de Cuenta Creada Correctamente'
+            'text' => $msg
         ]);
     }
 
-    public function mostrarCuentas(TipoPagos $tipo_pago){
+    public function mostrarCuentas(TipoPagos $tipo_pago)
+    {
         //dd($tipo_pago);
         $this->idTipoPago = $tipo_pago->id;
-        $this->title2 = 'AÑADIR CUENTAS BANCARIAS EN: '.$tipo_pago->nombre_tipo_pago;
+        $this->title2 = 'AÑADIR CUENTAS BANCARIAS EN: ' . $tipo_pago->nombre_tipo_pago;
     }
 
-    public function guardarCuentaBancaria(){
+    public function guardarCuentaBancaria()
+    {
         //Verificar si ya se hicieron depósitos y ya no se edita
         $cuenta_pagos = CuentaPagos::create([
-            'numero_cuenta' => $this->numero_cuenta, 
+            'numero_cuenta' => $this->numero_cuenta,
             'tipo_pagos_id' => $this->idTipoPago
         ]);
     }
 
+    public function Edit(TipoPagos $tipo_pago)
+    {
+        //dd($tipo_pago);
+        $this->idTipoPago = $tipo_pago->id;
+        $this->nombre_tipo_pago = $tipo_pago->nombre_tipo_pago;
+        $this->title = 'EDITAR TIPO DE PAGO';
 
+        $this->emit('show-modal', 'Edicion de Mapas');
+    }
 
+    public function deleteConfirm($id)
+    {
+        $this->dispatchBrowserEvent('swal-confirmTipoCuentas', [
+            'title' => 'Deseas Eliminar el Tipo de Pago?',
+            'icon' => 'warning',
+            'id' => $id
+        ]);
+    }
+
+    public function deleteTipoPago(TipoPagos $tipo_pago){
+        //dd($tipo_pago);
+        $pago = CuentaPagos::where('tipo_pagos_id', $tipo_pago->id)->get();
+        if (count($pago) > 0) {
+            # code...
+            $title='ERROR!';
+            $icon='error';
+            $msg = 'No se puede Eliminar el Tipo De pago porque está vinculada a una cuenta.';
+        } else {
+            # code...
+            $title='MUY BIEN !';
+            $tipo_pago->delete();
+            $icon ='success';
+            $msg = 'Tipo de Pago Eliminada Correctamente';
+        }
+        
+        $this->dispatchBrowserEvent('swal', [
+            'title' => $title,
+            'icon' => $icon,
+            'text' => $msg
+        ]);
+    }
 }
--- resources/views/livewire/reservas-admin/tipo-pagos-cuentas/tipo-pagos-cuentas.blade.php
+++ resources/views/livewire/reservas-admin/tipo-pagos-cuentas/tipo-pagos-cuentas.blade.php
@@ -33,10 +33,18 @@
                                     <td>{{ $t->nombre_tipo_pago }}</td>
 
                                     <td>
+                                        <button id="edit" wire:click="Edit({{ $t->id }})"
+                                            class="btn btn-warning btn-sm">
+                                            <span class="fa fa-pencil-square-o"></span>
+                                        </button>
                                         @if ($t->id != 1)
-                                            <button wire:click="mostrarCuentas({{ $t->id }})"
-                                                title="Ver Atractivos" class="btn btn-primary btn-sm">
+                                            <button wire:click="mostrarCuentas({{ $t->id }})" title="Ver Cuentas"
+                                                class="btn btn-primary btn-sm">
                                                 <span class="fas fa-eye"></span>
+                                            </button>
+                                            <button id="delete" class="btn btn-danger btn-sm"
+                                                wire:click="deleteConfirm({{ $t->id }})" title="Eliminar Mapa">
+                                                <span class="fa fa-trash"></span>
                                             </button>
                                         @endif
 
@@ -61,11 +69,17 @@
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <button type="button" class="btn btn-primary btn-rounded btn-sm" data-toggle="modal"
-                                data-target="#modal-cuentas-bancarias">
-                                Cuentas Bancarias
-                            </button>
+                            @if ($idTipoPago)
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm" data-toggle="modal"
+                                    data-target="#modal-cuentas-bancarias">
+                                    Cuentas Bancarias
+                                </button>
+                            @endif
+
                         </div>
+                    </div>
+                    <div wire:loading wire:target="mostrarCuentas" class="alert alert-primary" role="alert">
+                        <a href="#!" class="alert-link">Cargando ...</a>
                     </div>
                     <table class="table table-hover">
                         <thead>
@@ -97,7 +111,7 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">{{ $title }}</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -117,9 +131,15 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
-                    <button type="button" wire:click="saveTipoPago" wire:loading.attr="disabled"
-                        class="btn btn-primary">Guardar</button>
+                    <button type="button" class="btn btn-danger btn-rounded" data-dismiss="modal">Cerrar</button>
+                    @if ($idTipoPago)
+                        <button type="button" wire:click="saveTipoPago" wire:loading.attr="disabled"
+                            class="btn btn-primary btn-rounded">Actualizar</button>
+                    @else
+                        <button type="button" wire:click="saveTipoPago" wire:loading.attr="disabled"
+                            class="btn btn-primary btn-rounded">Guardar</button>
+                    @endif
+
                 </div>
             </div>
         </div>
@@ -128,8 +148,8 @@
 
 
     <!-- Modal -->
-    <div wire:ignore.self class="modal fade" id="modal-cuentas-bancarias" data-backdrop="static" data-keyboard="false"
-        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div wire:ignore.self class="modal fade" id="modal-cuentas-bancarias" data-backdrop="static"
+        data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -154,11 +174,54 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
-                    <button type="button" wire:click="guardarCuentaBancaria" class="btn btn-primary">Guardar</button>
+                    <button type="button" wire:click="guardarCuentaBancaria"
+                        class="btn btn-primary">Guardar</button>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        window.addEventListener('swal-confirmTipoCuentas', event => {
+            Swal.fire({
+                title: event.detail.title,
+                icon: event.detail.icon,
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, quiero eliminarlo!',
+                cancelButtonText: 'Cancelar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Livewire.emitTo('reservas-admin.tipo-pagos-cuentas.tipo-pagos-cuentas', 'deleteTipoPago',
+                        event.detail
+                        .id);
+                }
+            })
+        });
+    </script>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            //Lo que llega de CategoriesController
+            window.livewire.on('show-modal', msg => {
+                $('#modal_tipo_pago').modal('show')
+            });
+            window.livewire.on('close-modal', msg => {
+                $('#modal_tipo_pago').modal('hide')
+            });
+            window.livewire.on('show-modal-atractivos', msg => {
+                $('#modal-atractivo').modal('show')
+            });
+            window.livewire.on('close-modal-atractivos', msg => {
+                $('#modal-atractivo').modal('hide')
+            });
+            window.livewire.on('category-updated', msg => {
+                $('#theModal').modal('hide')
+            });
+        });
+    </script>
+
 
     @include('common.alerts')
 </div>
