c406cb6d266d1c94bdeb04b5d76754088b643451
Carlos Alvarado Robles <carlos2000emilioa@gmail.com>
2023-03-12 16:40:06 -0500

    Interfaz de Añadir Pagos en la Parte Pública

--- resources/views/perfil_cliente/paquetes_comprados.blade.php
+++ resources/views/perfil_cliente/paquetes_comprados.blade.php
@@ -6,73 +6,136 @@
         <div class="container box_1170">
             <div class="section-top-border">
                 <h3 class="mb-30">Mis compras</h3>
-                <div class="progress-table-wrap">
-                    <div class="progress-table">
-                        <div class="table-head">
-                            <div class="country">Paquete</div>
-                            <div class="country">Fecha</div>
-                            <div class="country">Estado</div>
-                            <div class="country">Monto</div>
-                            <div class="country">Acciones</div>
-                        </div>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">PAQUETE</th>
+                            <th scope="col">Fecha Reservada</th>
+                            <th scope="col">Estado</th>
+                            <th scope="col">MONTO PAGADO (S/.)</th>
+                            <th scope="col">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                         @foreach ($paquetes_comprados as $p)
-                            <div class="table-row">
-                                <div class="country"> {{ $p->nombre }}</div>
-                                <div class="serial">{{ $p->fecha_reserva }}</div>
-                                <div class="serial">
-                                    {{ $p->nombre_estado }}
-                                    {{-- @if ($p->nombre_estados == 'COMPLETADO')
-                                       Medium
-                                    @else
-                                        <a href="#"
-                                            class="genric-btn danger small circle arrow">{{ $p->nombre_estado }}</a>
-                                    @endif --}}
-                                </div>
-                                <div class="serial">
-                                    {{ $p->pago }}
-                                </div>
-                                <div class="percentage">
-                                    <a href="#" class="genric-btn success small circle arrow">Añadir Pago</a>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>{{ $p->nombre }}</td>
+                                <td>{{ $p->fecha_reserva }}</td>
+                                <td>{{ $p->nombre_estado }}</td>
+                                <td>{{ $p->pago }}</td>
+                                <td>
+                                    <a id="modal-418541" href="#modal_page" role="button"
+                                        class="genric-btn success small circle arrow" data-toggle="modal">Añadir Pago</a>
+                                    {{-- <a href="#" class="">Añadir Pago</a>
                                     <a href="#" class="genric-btn info small circle arrow">Medium</a>
-                                    <a href="#" class="genric-btn danger small circle arrow">Medium</a>
-                                </div>
-                            </div>
+                                    <a href="#" class="genric-btn danger small circle arrow">Medium</a> --}}
+                                </td>
+                            </tr>
                         @endforeach
 
-                    </div>
-                </div>
+
+                    </tbody>
+                </table>
             </div>
         </div>
 
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <a id="modal-418541" href="#modal-container-418541" role="button" class="btn"
-                        data-toggle="modal">Launch demo modal</a>
 
-                    <div class="modal fade" id="modal-container-418541" role="dialog" aria-labelledby="myModalLabel"
-                        aria-hidden="true">
-                        <div class="modal-dialog" role="document">
+                    <div class="modal fade" id="modal_page" data-backdrop="static" data-keyboard="false" role="dialog"
+                        aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h5 class="modal-title" id="myModalLabel">
-                                        Modal title
+                                        AÑADIR PAGOS RESTANTES
                                     </h5>
                                     <button type="button" class="close" data-dismiss="modal">
                                         <span aria-hidden="true">×</span>
                                     </button>
                                 </div>
                                 <div class="modal-body">
-                                    ...
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <form class="form-contact contact_form" action="google  " method="post"
+                                                id="contactForm" novalidate="novalidate">
+                                                <div class="row">
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group">
+                                                            <label for="exampleInputEmail1">Monto de Pago (S/.)</label>
+                                                            <input class="form-control valid" name="name" id="name"
+                                                                type="password" onfocus="this.placeholder = ''"
+                                                                onblur="this.placeholder = 'Enter your name'"
+                                                                placeholder="Ingrese su Contraseña Actual">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group">
+                                                            <label for="exampleInputEmail1">Fecha de Pago</label>
+                                                            <input class="form-control valid" name="password" id="name"
+                                                                type="date" onfocus="this.placeholder = ''"
+                                                                onblur="this.placeholder = 'Enter your name'"
+                                                                placeholder="Ingrese la contraseña nueva">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group">
+                                                            <label for="exampleInputEmail1">Nº de Operación</label>
+                                                            <input class="form-control valid" name="name" id="name"
+                                                                type="password" onfocus="this.placeholder = ''"
+                                                                onblur="this.placeholder = 'Enter your name'"
+                                                                placeholder="Repita nuevamente la contraseña Ingresada con anterioridad">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-sm-6">
+                                                        <div class="form-group">
+                                                            <label for="exampleInputEmail1">Archivo</label>
+                                                            <input class="form-control valid" name="name" id="name"
+                                                                type="file" onfocus="this.placeholder = ''"
+                                                                onblur="this.placeholder = 'Enter your name'"
+                                                                placeholder="Repita nuevamente la contraseña Ingresada con anterioridad">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-sm-12">
+                                                        <div class="form-group">
+                                                            <label for="exampleInputEmail1">Medio de Pago</label>
+                                                            <div class="search_wrap input_field">
+                                                                <select style="display: inline;">
+                                                                    <option data-display="...Seleccione...">...Seleccione...</option>
+                                                                    <option value="1">Some option</option>
+                                                                    <option value="2">Another option</option>
+                                                                </select>
+                                                            </div>
+
+
+                                                            {{-- <input class="form-control valid" name="name" id="name"
+                                                                type="password" onfocus="this.placeholder = ''"
+                                                                onblur="this.placeholder = 'Enter your name'"
+                                                                placeholder="Repita nuevamente la contraseña Ingresada con anterioridad"> --}}
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                                
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="modal-footer">
-
-                                    <button type="button" class="btn btn-primary">
-                                        Save changes
-                                    </button>
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                                        Close
+                                        Cerrar
                                     </button>
+                                    <button type="button" class="btn btn-primary">
+                                        Guardar
+                                    </button>
+
                                 </div>
                             </div>
 
