b8de35803579ad4b1f28ddfe4fcdb7f195d7622c
Carlos Alvarado Robles <carlos2000emilioa@gmail.com>
2023-04-30 10:28:54 -0500

    Edición y Eliminación de Cuentas Bancarias de un proveedor

--- app/Http/Livewire/ProveedoresAdmin/Proveedores/Proveedores/CuentasBancarias.php
+++ app/Http/Livewire/ProveedoresAdmin/Proveedores/Proveedores/CuentasBancarias.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\ProveedoresAdmin\Proveedores\Proveedores;
 
 use App\Models\Pedidos\Bancos;
 use App\Models\Pedidos\CuentaProveedorBancos;
+use App\Models\Pedidos\PagoProveedores;
 use App\Models\Pedidos\Proveedores;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
@@ -11,14 +12,16 @@ use Livewire\Component;
 class CuentasBancarias extends Component
 {
     public $proveedor;
-    public $numero_cuenta, $estado, $banco;
+    public $idCuentaProveedorBanco, $numero_cuenta, $estado, $banco;
     public $title = 'CREAR CUENTAS BANCARIAS';
 
-    public function resetUI(){
-        $this->reset(['numero_cuenta', 'estado', 'banco']);
+    public function resetUI()
+    {
+        $this->reset(['idCuentaProveedorBanco', 'numero_cuenta', 'estado', 'banco']);
     }
 
-    public function mount(Proveedores $proveedor){
+    public function mount(Proveedores $proveedor)
+    {
         $this->proveedor = $proveedor;
     }
 
@@ -28,20 +31,24 @@ class CuentasBancarias extends Component
         $bancos = Bancos::all(['id', 'nombre_banco', 'direccion']);
         $cuentas = DB::table('bancos as b')
             ->join('cuenta_proveedor_bancos as cpb', 'b.id', '=', 'cpb.bancos_id')
+            ->where('cpb.proveedores_id', $this->proveedor->id)
             ->select(
-                'b.nombre_banco', 
+                'b.nombre_banco',
                 //'b.direccion', 
-                'cpb.numero_cuenta', 
-                'cpb.estado', 
+                'cpb.numero_cuenta',
+                'cpb.estado',
                 'cpb.id'
             )
             ->get();
-            
-        return view('livewire.proveedores-admin.proveedores.proveedores.cuentas-bancarias', 
-        compact('bancos','cuentas'));
+
+        return view(
+            'livewire.proveedores-admin.proveedores.proveedores.cuentas-bancarias',
+            compact('bancos', 'cuentas')
+        );
     }
 
-    public function saveCuenta(){
+    public function saveCuenta()
+    {
         $this->validate(
             [
                 'numero_cuenta' => 'required|string|min:3',
@@ -49,21 +56,69 @@ class CuentasBancarias extends Component
                 'banco' => 'required|numeric|min:1',
             ]
         );
+        $title = 'MUY BIEN';
+        $text = 'Información de la Cuenta Bancaria Registrada Correctamente.';
+        $icon = 'success';
 
-        $banco = CuentaProveedorBancos::create(
-            [
-                'numero_cuenta' => $this->numero_cuenta, 
-                'estado' => $this->estado, 
-                'proveedores_id' => $this->proveedor->id, 
-                'bancos_id' => $this->banco
-            ]
-        );
+        if ($this->idCuentaProveedorBanco) {
+            $cuentas = CuentaProveedorBancos::findOrFail($this->idCuentaProveedorBanco);
+            $cuentas->numero_cuenta = $this->numero_cuenta;
+            $cuentas->estado = $this->estado;
+            $cuentas->bancos_id = $this->banco;
+            $cuentas->save();
+            $text = 'Información de la Cuenta Bancaria Actualizada Correctamente.';
+            $this->emit('close-modal');
+        } else {
+            $cuentas = CuentaProveedorBancos::create(
+                [
+                    'numero_cuenta' => $this->numero_cuenta,
+                    'estado' => $this->estado,
+                    'proveedores_id' => $this->proveedor->id,
+                    'bancos_id' => $this->banco
+                ]
+            );
+        }
 
         $this->resetUI();
-        $this->emit('alert', 'MUY BIEN', 'success', 'Cuenta Bancaria Registrada Correctamente.');
+        $this->emit('alert', $title, $icon, $text);
     }
 
-    public function Edit(){
+    public function Edit(CuentaProveedorBancos $cuentas)
+    {
+        $this->idCuentaProveedorBanco = $cuentas->id;
+        $this->numero_cuenta = $cuentas->numero_cuenta;
+        $this->estado = $cuentas->estado;
+        $this->banco = $cuentas->bancos_id;
+        $this->emit('show-modal');
+    }
 
+    public function deleteConfirm($id)
+    {
+        $this->dispatchBrowserEvent('swal-confirm-cuentas', [
+            'title' => 'Está seguro que desea eliminar la Cuenta Bancaria del Proveedor ?',
+            'icon' => 'warning',
+            'id' => $id
+        ]);
+    }
+
+    protected $listeners = ['deleteCuentaBancaria'];
+    public function deleteCuentaBancaria(CuentaProveedorBancos $cuentaProveedorBancos)
+    {
+        // dd($proveedores);
+        $title = 'MUY BIEN!';
+        $icon = 'success';
+        $text = 'Cuenta Bancaria del Proveedor Eliminado Correctamente.';
+
+        $pago_proveedores = PagoProveedores::where('cuenta_proveedor_bancos_id', $cuentaProveedorBancos->id)->get();
+
+        if (count($pago_proveedores) > 0) {
+            $title = 'ERROR !';
+            $icon = 'error';
+            $text = 'No se puede Eliminar la Cuenta del Proveedor, porque ya se le registraron pagos en ella.';
+            $this->emit('alert', $title, $icon, $text);
+        } else {
+            $cuentaProveedorBancos->delete();
+            $this->emit('alert', $title, $icon, $text);
+        }
     }
 }
--- resources/views/livewire/proveedores-admin/proveedores/proveedores/cuentas-bancarias.blade.php
+++ resources/views/livewire/proveedores-admin/proveedores/proveedores/cuentas-bancarias.blade.php
@@ -24,8 +24,8 @@
                             </div>
                         </div>
                         <div class="col-md-3">
-                            <a id="modal-532427" href="#modal-traslado-viajes" role="button" class="btn btn-rounded"
-                                data-toggle="modal">CREAR NUEVA CUENTA</a>
+                            <button id="modal-532427" href="#modal_cuentas_bancarias" role="button"
+                                class="btn btn-rounded" data-toggle="modal">CREAR NUEVA CUENTA</button>
                         </div>
                     </div>
                     <table class="table table-hover">
@@ -42,27 +42,26 @@
                                 <tr>
                                     <td>{{ $c->nombre_banco }}</td>
                                     <td>{{ $c->numero_cuenta }}</td>
-                                    <td>{{ $c->estado }}</td>
                                     <td>
-                                        <button id="edit" title="Editar Información de Proveedor"
-                                            wire:click="Edit()" class="btn btn-warning btn-sm">
+                                        @if ($c->estado == 1)
+                                            <span class="label label-success">ACTIVA</span>
+                                        @else
+                                            <span class="label label-danger">NO ACTIVA</span>
+                                        @endif
+
+                                    </td>
+                                    <td>
+                                        <button title="Editar Información de la Cuenta Bancaria"
+                                            wire:click="Edit({{ $c->id }})" wire:loading.attr="disabled"
+                                            class="btn btn-warning btn-sm">
                                             <span class="fa fa-pencil-square-o"></span>
                                         </button>
-                                        <button id="view" wire:click="mostrarAtractivosDelLugar()"
-                                            title="Eliminar Proveedor" class="btn btn-danger btn-sm">
+                                        <button title="Eliminar Cuenta Bancaria del Proveedor"
+                                            wire:click="deleteConfirm({{ $c->id }})" wire:loading.attr="disabled"
+                                            class="btn btn-danger btn-sm">
                                             <span class="fa fa-trash"></span>
                                         </button>
-                                        <button id="delete" title="Añadir Cuentas Bancarias"
-                                            data-target="#exampleModal" data-toggle="modal"
-                                            class="btn btn-success btn-sm" wire:click="deleteConfirm()">
-                                            <i class="fas fa-plus-circle"></i>
-                                        </button>
-                                        <button id="view" title="Añadir Pedidos de Proveedor"
-                                            data-target="#exampleModal" data-toggle="modal"
-                                            wire:click="mostrarAtractivosDelLugar()" title="Ver Atractivos"
-                                            class="btn btn-danger btn-sm">
-                                            <i class="fas fa-minus"></i>
-                                        </button>
+
                                     </td>
                                 </tr>
                             @endforeach
@@ -78,14 +77,14 @@
 
 
     <!--MODAL --->
-    <div class="modal fade" wire:ignore.self data-backdrop="static" data-keyboard="false" id="modal-traslado-viajes"
+    <div class="modal fade" wire:ignore.self data-backdrop="static" data-keyboard="false" id="modal_cuentas_bancarias"
         role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <form wire:submit.prevent="saveCuenta">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="myModalLabel">
-                            {{$title}}
+                            {{ $title }}
                         </h5>
                         <button type="button" class="close" data-dismiss="modal">
                             <span aria-hidden="true">×</span>
@@ -137,12 +136,18 @@
                     </div>
 
                     <div class="modal-footer">
-                        <button type="button" wire:click.prevent="resetUI()" class="btn btn-rounded btn-danger" data-dismiss="modal">
+                        <button type="button" wire:click.prevent="resetUI()" class="btn btn-rounded btn-danger"
+                            data-dismiss="modal">
                             Cerrar
                         </button>
 
                         <button type="submit" class="btn btn-rounded btn-primary">
-                            Guardar
+                            @if ($idCuentaProveedorBanco)
+                                Actualizar
+                            @else
+                                Guardar
+                            @endif
+
                         </button>
 
                     </div>
@@ -157,16 +162,32 @@
     @livewire('administrate-commons.alerts')
 
     <script>
+        window.addEventListener('swal-confirm-cuentas', event => {
+            Swal.fire({
+                title: event.detail.title,
+                icon: event.detail.icon,
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, quiero eliminarlo!',
+                cancelButtonText: 'Cancelar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Livewire.emitTo('proveedores-admin.proveedores.proveedores.cuentas-bancarias',
+                        'deleteCuentaBancaria',
+                        event.detail
+                        .id);
+                }
+            })
+        });
+
         document.addEventListener('DOMContentLoaded', function() {
             //Lo que llega de CategoriesController
             window.livewire.on('show-modal', msg => {
-                $('#modalMontoArriero').modal('show')
+                $('#modal_cuentas_bancarias').modal('show')
             });
-            window.livewire.on('fecha-itinerario-guarded', msg => {
-                $('#modalMontoArriero').modal('hide')
-            });
-            window.livewire.on('category-updated', msg => {
-                $('#theModal').modal('hide')
+            window.livewire.on('close-modal', msg => {
+                $('#modal_cuentas_bancarias').modal('hide')
             });
         });
     </script>
